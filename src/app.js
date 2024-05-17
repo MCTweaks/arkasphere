3a72b6a8a6a8e00bba58190dcd1ebccbcc3a8286
HetMasterTje <98865572+HetMasterTje@users.noreply.github.com>
2024-05-17 19:52:34 -0300

    Update App.js

--- src/app.js
+++ src/app.js
@@ -12,10 +12,12 @@ app.use(bodyParser.json());
 
 // The Proprietary module is not required.
 try {
-    const proprietary = require('./net.hetmastertje/Main.js');
-    proprietary(app);
+    const netHetMasterTje = require('./net.hetmastertje/Main.js');
+    const comArkaSphere = require('./com.arkasphere/Main.js');
+    netHetMasterTje(app);
+    comArkaSphere(app);
 } catch (error) {
-    console.error("Error loading proprietary module:", error);
+    console.error("Error loading modules:", error);
     // Handle the error gracefully, or just continue without it.
 }
 
